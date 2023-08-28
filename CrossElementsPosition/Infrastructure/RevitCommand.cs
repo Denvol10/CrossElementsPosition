1833cd4ef5c21e5d2e05fbf35363f02f8a9886cd
Денис Щербатюк <denvol100@gmail.com>
2023-08-28 11:59:47 +0300

    First setup

--- CrossElementsPosition/Infrastructure/RevitCommand.cs
+++ CrossElementsPosition/Infrastructure/RevitCommand.cs
@@ -9,9 +9,9 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using Autodesk.Revit.DB.Architecture;
-using RevitWPFTemplate.ViewModels;
+using CrossElementsPosition.ViewModels;
 
-namespace RevitWPFTemplate.Infrastructure
+namespace CrossElementsPosition.Infrastructure
 {
     [Transaction(TransactionMode.Manual)]
     [Regeneration(RegenerationOption.Manual)]
